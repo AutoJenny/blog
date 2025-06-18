cc1a109f5c774fe4628b71f58c13dd0f4afdef67
AutoJenny <github@nickfiddes.com>
2025-06-18 15:55:26 +0100

    Canonical minimal nav module: firewalled, no cruft, ready for modular integration

--- migrations/20240528_add_provider_id_to_llm_action.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add provider_id to llm_action
-ALTER TABLE llm_action ADD COLUMN provider_id INTEGER;
-UPDATE llm_action SET provider_id = 1 WHERE provider_id IS NULL;
-ALTER TABLE llm_action ALTER COLUMN provider_id SET NOT NULL;
-ALTER TABLE llm_action ADD CONSTRAINT fk_llm_action_provider FOREIGN KEY (provider_id) REFERENCES llm_provider(id); 
